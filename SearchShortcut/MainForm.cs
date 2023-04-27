e4346f24e5e109fa62adaf44d1cc4a1a016976e8
Victor <publicdomainvic@gmail.com>
2023-04-27 17:26:25 -0400

    Add OnSourceCodeGithubcomToolStripMenuItemClick()

--- SearchShortcut/MainForm.cs
+++ SearchShortcut/MainForm.cs
@@ -408,7 +408,8 @@ namespace SearchShortcut
         /// <param name="e">Event arguments.</param>
         private void OnSourceCodeGithubcomToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Open GitHub repository
+            Process.Start("https://github.com/publicdomain/search-shortcut");
         }
 
         /// <summary>
